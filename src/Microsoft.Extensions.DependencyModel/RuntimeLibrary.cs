19837b4d47143bb9ccfa688c9b6123b72ab91db6
Joel Verhagen <jver@microsoft.com>
2016-08-11 09:46:19 -0700

    Add support for the "path" property in the .deps.json file

--- src/Microsoft.Extensions.DependencyModel/RuntimeLibrary.cs
+++ src/Microsoft.Extensions.DependencyModel/RuntimeLibrary.cs
@@ -19,7 +19,22 @@ namespace Microsoft.Extensions.DependencyModel
             IEnumerable<ResourceAssembly> resourceAssemblies,
             IEnumerable<Dependency> dependencies,
             bool serviceable)
-            : base(type, name, version, hash, dependencies, serviceable)
+            : this(type, name, version, hash, runtimeAssemblyGroups, nativeLibraryGroups, resourceAssemblies, dependencies, serviceable, path: null)
+        {
+        }
+
+        public RuntimeLibrary(
+            string type,
+            string name,
+            string version,
+            string hash,
+            IReadOnlyList<RuntimeAssetGroup> runtimeAssemblyGroups,
+            IReadOnlyList<RuntimeAssetGroup> nativeLibraryGroups,
+            IEnumerable<ResourceAssembly> resourceAssemblies,
+            IEnumerable<Dependency> dependencies,
+            bool serviceable,
+            string path)
+            : base(type, name, version, hash, dependencies, serviceable, path)
         {
             if (runtimeAssemblyGroups == null)
             {
